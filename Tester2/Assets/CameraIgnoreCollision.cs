a0d4ea58716e4356d3a0e52ef0c501383d83d403
Simon Asholt Norup <wormple12@hotmail.com>
2020-04-15 12:23:34 +0200

    failed attempt to fix camera collision - we'll continue without

---
+++ Tester2/Assets/CameraIgnoreCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraIgnoreCollision : MonoBehaviour {
+
+    public Collider bird;
+    public Collider camera;
+
+    // Start is called before the first frame update
+    void Start () {
+        Physics.IgnoreCollision (bird, camera);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
